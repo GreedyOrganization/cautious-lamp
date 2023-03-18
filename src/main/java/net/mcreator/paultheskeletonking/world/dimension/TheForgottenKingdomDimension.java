b8d28a43a77bf3bb4ca285bd03765530ddd5cc7d
Greedy-Noob <siemaelo321890@gmail.com>
2023-03-18 23:38:24 +0100

    Biome

--- src/main/java/net/mcreator/paultheskeletonking/world/dimension/TheForgottenKingdomDimension.java
+++ src/main/java/net/mcreator/paultheskeletonking/world/dimension/TheForgottenKingdomDimension.java
@@ -1,17 +1,24 @@
 
 package net.mcreator.paultheskeletonking.world.dimension;
 
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+
 @Mod.EventBusSubscriber
 public class TheForgottenKingdomDimension {
-
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class Fixers {
-
 		@SubscribeEvent
 		@OnlyIn(Dist.CLIENT)
 		public static void registerDimensionSpecialEffects(FMLClientSetupEvent event) {
 			DimensionSpecialEffects customEffect = new DimensionSpecialEffects(Float.NaN, true, DimensionSpecialEffects.SkyType.NONE, false, false) {
-
 				@Override
 				public Vec3 getBrightnessDependentFogColor(Vec3 color, float sunHeight) {
 					return new Vec3(0.2, 0.2, 0.2);
@@ -21,11 +28,8 @@ public class TheForgottenKingdomDimension {
 				public boolean isFoggyAt(int x, int y) {
 					return true;
 				}
-
 			};
 			event.enqueueWork(() -> DimensionSpecialEffects.EFFECTS.put(new ResourceLocation("paultheskeletonking:the_forgotten_kingdom"), customEffect));
 		}
-
 	}
-
 }
